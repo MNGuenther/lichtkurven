130817effdaafb87871b6572c326d927a9888e54
MNGuenther <mg719@cam.ac.uk>
2018-10-11 20:26:23 -0400

    index_transits now automatically sets the epoch to the start of the data set

--- index_transits.py
+++ index_transits.py
@@ -22,7 +22,19 @@ from .utils import mask_ranges
 
 
 
-def index_transits(dic, obj_nr=None):
+def get_first_epoch(time, epoch, period):    
+    '''
+    place the first_epoch at the start of the data to avoid luser mistakes
+    '''
+    start = np.nanmin( time )
+    first_epoch = 1.*epoch
+    if start<=first_epoch: first_epoch -= int(np.round((first_epoch-start)/period)) * period
+    else: first_epoch += int(np.round((start-first_epoch)/period)) * period
+    return first_epoch    
+    
+    
+
+def index_transits(time, epoch, period, width):
     """
     Returns:
     --------
@@ -31,34 +43,21 @@ def index_transits(dic, obj_nr=None):
     ind_out : array
         indices of points out of transit
     """
+    epoch = get_first_epoch(time, epoch, period)
+    N = int( 1. * ( time[-1] - epoch ) / period ) + 1
     
-    if obj_nr is not None:
-        N = int( 1. * ( dic['TIME'][obj_nr][-1] - dic['EPOCH'][obj_nr] ) / dic['PERIOD'][obj_nr] ) + 1
-        tmid = np.array( [ dic['EPOCH'][obj_nr] + i * dic['PERIOD'][obj_nr] for i in range(N) ] )
-        
-        _, ind_tr, mask_tr = mask_ranges( dic['TIME'][obj_nr], tmid - dic['WIDTH'][obj_nr]/2., tmid + dic['WIDTH'][obj_nr]/2. )
-        _, ind_tr_half, _ = mask_ranges( dic['TIME'][obj_nr], tmid - dic['WIDTH'][obj_nr]/4., tmid + dic['WIDTH'][obj_nr]/4. )
-        _, ind_tr_double, _ = mask_ranges( dic['TIME'][obj_nr], tmid - dic['WIDTH'][obj_nr], tmid + dic['WIDTH'][obj_nr] )
-              
-        ind_out = np.arange( len(dic['TIME'][obj_nr]) )[ ~mask_tr ]
-
-    else:
-        N = int( 1. * ( dic['TIME'][-1] - dic['EPOCH'] ) / dic['PERIOD'] ) + 1
-        
-        tmid = np.array( [ dic['EPOCH'] + i * dic['PERIOD'] for i in range(N) ] )
-        
-        _, ind_tr, mask_tr = mask_ranges( dic['TIME'], tmid - dic['WIDTH']/2., tmid + dic['WIDTH']/2. )
-        _, ind_tr_half, _ = mask_ranges( dic['TIME'], tmid - dic['WIDTH']/4., tmid + dic['WIDTH']/4. )
-        _, ind_tr_double, _ = mask_ranges( dic['TIME'], tmid - dic['WIDTH'], tmid + dic['WIDTH'] )
-              
-        ind_out = np.arange( len(dic['TIME']) )[ ~mask_tr ]
+    tmid = np.array( [ epoch + i * period for i in range(N) ] )
+    
+    _, ind_tr, mask_tr = mask_ranges( time, tmid - width/2., tmid + width/2. )
+    ind_out = np.arange( len(time) )[ ~mask_tr ]
 
     return ind_tr, ind_out 
     
     
 
 #::: for binaries, mark the primary and secondary eclipse
-def index_eclipses(dic, obj_nr=None):
+#TODO: implement non-circular orbits
+def index_eclipses(time, epoch, period, width_1, width_2):
     """
     Returns:
     --------
@@ -71,30 +70,16 @@ def index_eclipses(dic, obj_nr=None):
     
     ! this assumes circular orbits !
     """
-    #TODO: implement non-circular orbits
+    epoch = get_first_epoch(time, epoch, period)
+    N = int( 1. * ( time[-1] - epoch ) / period ) + 1
+        
+    tmid_ecl1 = np.array( [ epoch +             i * period  for i in range(N) ] )
+    tmid_ecl2 = np.array( [ epoch - period/2. + i * period  for i in range(N+1) ] )
     
-    
-    if obj_nr is not None:
-        print('Not currently implemented.')
-        return None
-
-
-    else:
-        N = int( 1. * ( dic['TIME'][-1] - dic['EPOCH'] ) / dic['PERIOD'] ) + 1
+    _, ind_ecl1, mask_ecl1 = mask_ranges( time, tmid_ecl1 - width_1/2., tmid_ecl1 + width_1/2. )           
+    _, ind_ecl2, mask_ecl2 = mask_ranges( time, tmid_ecl2 - width_2/2., tmid_ecl2 + width_2/2. )
         
-        tmid_ecl1 = np.array( [ dic['EPOCH'] +                    i * dic['PERIOD']  for i in range(N) ] )
-        tmid_ecl2 = np.array( [ dic['EPOCH'] - dic['PERIOD']/2. + i * dic['PERIOD']  for i in range(N+1) ] )
-        
-        _, ind_ecl1,        mask_ecl1 = mask_ranges( dic['TIME'], tmid_ecl1 - dic['WIDTH']/2., tmid_ecl1 + dic['WIDTH']/2. )
-#        _, ind_ecl1_half,   _         = mask_ranges( dic['TIME'], tmid_ecl1 - dic['WIDTH']/4., tmid_ecl1 + dic['WIDTH']/4. )
-#        _, ind_ecl1_double, _         = mask_ranges( dic['TIME'], tmid_ecl1 - dic['WIDTH'],    tmid_ecl1 + dic['WIDTH']    )
-              
-        _, ind_ecl2,        mask_ecl2 = mask_ranges( dic['TIME'], tmid_ecl2 - dic['WIDTH_2']/2., tmid_ecl2 + dic['WIDTH_2']/2. )
-#        _, ind_ecl2_half,   _         = mask_ranges( dic['TIME'], tmid_ecl2 - dic['WIDTH']/4., tmid_ecl2 + dic['WIDTH']/4. )
-#        _, ind_ecl2_double, _         = mask_ranges( dic['TIME'], tmid_ecl2 - dic['WIDTH'],    tmid_ecl2 + dic['WIDTH']    )
-        
-        ind_out = np.arange( len(dic['TIME']) )[ ~(mask_ecl1 | mask_ecl2) ]
-
+    ind_out = np.arange( len(time) )[ ~(mask_ecl1 | mask_ecl2) ]
 
     return ind_ecl1, ind_ecl2, ind_out
     
